fbd8768d2f73c1a4fdbf65510eae1ec9f4490973
Mathew Benson <mbenson@windhoverlabs.com>
2016-09-27 20:52:45 -0500

    Updated submodules.  Yamcs and Yamcs Studio appear to build with this configuration.

--- Vagrantfile
+++ Vagrantfile
@@ -66,5 +66,6 @@ Vagrant.configure(2) do |config|
   # Enable provisioning with a shell script. Additional provisioners such as
   # Puppet, Chef, Ansible, Salt, and Docker are also available. Please see the
   # documentation for more information about their specific syntax and use.
-  config.vm.provision "shell", privileged: false, path: "scripts/ubuntu1604-64.sh" 
+  config.vm.provision "shell", privileged: false, 
+    inline: "/vagrant/scripts/ubuntu1604-64.sh" 
 end
--- scripts/ubuntu1604-64.sh
+++ scripts/ubuntu1604-64.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 cd "$(dirname "$0")"
+cd ..
 WORKSHOP_BASEDIR=$PWD
 
 # Source the main setvars.sh script to get all the CFS environment variables
@@ -8,23 +9,43 @@ WORKSHOP_BASEDIR=$PWD
 #source setvars.sh
 
 # Install the prereqs
-#sudo apt-get -y update
+export DEBIAN_FRONTEND=noninteractive
+sudo apt-get -y update
 sudo apt-get -y install eclipse eclipse-cdt g++ g++-multilib git gitk openjdk-8-jdk cinnamon maven libc-bin libc-dev-bin libc6-dev rpm nodejs-legacy npm alien
 sudo npm -g install bower gulp
 
+# Install Oracle JDK 8
+sudo apt-get install -y default-jre
+sudo apt-get install -y default-jdk
+sudo add-apt-repository -y ppa:webupd8team/java
+sudo apt-get -y update
+echo debconf shared/accepted-oracle-license-v1-1 select true | sudo debconf-set-selections
+echo debconf shared/accepted-oracle-license-v1-1 seen true | sudo debconf-set-selections
+sudo apt-get install -y oracle-java8-installer
+echo "export JAVA_HOME='/usr/lib/jvm/java-8-oracle'" >> ~/.bashrc
+source ~/.bashrc
+
+
 # Build YAMCS
 cd ${WORKSHOP_BASEDIR}/yamcs
-./make-rpm.sh
-mvn install -DskipTests=true -Dmaven.javadoc.skip=true -B -V
+mvn -Dsurefire.useFile=false test
+mvn install -DskipTests=true
 ./make-client-package.sh
+./make-rpm.sh
 sudo useradd -r yamcs
-sudo alien --scripts --install ~/rpmbuild/RPMS/noarch/yamcs-*
+sudo alien -c ~/rpmbuild/RPMS/noarch/yamcs-*
 sudo rm -Rf /opt/yamcs/cache /opt/yamcs/etc /opt/yamcs/log /opt/yamcs/mdb
 
-#sudo git clone --recursive ssh://bitbucket.windhoverlabs.com/scm/wor/yamcs.git /opt/yamcs
+# Build YAMCS Studio
+cd ${WORKSHOP_BASEDIR}/yamcs-studio
+ln -s ../yamcs yamcs
+#mvn install -DskipTests -f yamcs/pom.xml
+sed "s#REPLACE_WITH_LOCAL_P2_REPO#"`pwd`/css/local_p2_repository"#" css/settings_template.xml >css/settings.xml
+mvn verify -f yamcs-studio-osgi/pom.xml -s css/settings.xml -Pcss-for-yamcs,travis
+mvn verify -f yamcs-studio-tycho/pom.xml -s css/settings.xml -Pcss-for-yamcs,travis
 
 # Install YAMCS Studio
-#sudo wget https://github.com/yamcs/yamcs-studio/releases/download/v1.0.0-beta.29/yamcs-studio-1.0.0-SNAPSHOT-linux.gtk.x86_64.tar.gz -O - | sudo tar -xz -C /opt
+# sudo wget https://github.com/yamcs/yamcs-studio/releases/download/v1.0.0-beta.29/yamcs-studio-1.0.0-SNAPSHOT-linux.gtk.x86_64.tar.gz -O - | sudo tar -xz -C /opt
 #sudo mv /opt/yamcs-studio-1.0.0-SNAPSHOT /opt/yamcs-studio
 
 # Setup and build the softsim build
