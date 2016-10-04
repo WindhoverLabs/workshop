ecee72d2a9be3194efddae5260740b0385adc872
Mathew Benson <mbenson@windhoverlabs.com>
2016-10-04 16:51:33 -0500

    Updated scripts to install yamcs, yamcs studio, and yamcs cfs plugin.

--- scripts/ubuntu1604-64.sh
+++ scripts/ubuntu1604-64.sh
@@ -25,28 +25,31 @@ sudo apt-get install -y oracle-java8-installer
 echo "export JAVA_HOME='/usr/lib/jvm/java-8-oracle'" >> ~/.bashrc
 source ~/.bashrc
 
+# Make the desktop look good
+sudo apt-get install -y cinnamon-desktop-environment
+#  Hack to change desktop image
+#sudo cp /usr/share/backgrounds/Black_hole_by_Marek_Koteluk.jpg /usr/share/images/desktop-base/desktop-background
 
-# Build YAMCS
-#cd ${WORKSHOP_BASEDIR}/yamcs
-#mvn -Dsurefire.useFile=false test
-#mvn install -DskipTests=true
-#./make-client-package.sh
-#./make-rpm.sh
-#sudo useradd -r yamcs
-#sudo alien -c ~/rpmbuild/RPMS/noarch/yamcs-*
-#sudo rm -Rf /opt/yamcs/cache /opt/yamcs/etc /opt/yamcs/log /opt/yamcs/mdb
-
-# Build YAMCS Studio
-#cd ${WORKSHOP_BASEDIR}/yamcs-studio
-#ln -s ../yamcs yamcs
-##mvn install -DskipTests -f yamcs/pom.xml
-#sed "s#REPLACE_WITH_LOCAL_P2_REPO#"`pwd`/css/local_p2_repository"#" css/settings_template.xml >css/settings.xml
-#mvn verify -f yamcs-studio-osgi/pom.xml -s css/settings.xml -Pcss-for-yamcs,travis
-#mvn verify -f yamcs-studio-tycho/pom.xml -s css/settings.xml -Pcss-for-yamcs,travis
+# Install YAMCS
+wget http://www.windhoverlabs.com/releases/yamcs/yamcs-0.29.5+rbdbf495-10.noarch.rpm
+sudo useradd -r yamcs
+sudo alien --scripts -i yamcs-0.29.5+rbdbf495-10.noarch.rpm
+rm yamcs-0.29.5+rbdbf495-10.noarch.rpm
+sudo rm -Rf /opt/yamcs/cache
+sudo rm -Rf /opt/yamcs/etc
+sudo rm -Rf /opt/yamcs/log
+sudo rm -Rf /opt/yamcs/mdb
 
 # Install YAMCS Studio
-# sudo wget https://github.com/yamcs/yamcs-studio/releases/download/v1.0.0-beta.29/yamcs-studio-1.0.0-SNAPSHOT-linux.gtk.x86_64.tar.gz -O - | sudo tar -xz -C /opt
-#sudo mv /opt/yamcs-studio-1.0.0-SNAPSHOT /opt/yamcs-studio
+wget http://www.windhoverlabs.com/releases/yamcs-studio/yamcs-studio-1.0.0-SNAPSHOT-linux.gtk.x86_64.tar.gz
+tar -xzf yamcs-studio-1.0.0-SNAPSHOT-linux.gtk.x86_64.tar.gz
+sudo mv yamcs-studio-1.0.0-SNAPSHOT /opt/yamcs-studio
+sudo chown -R root:root /opt/yamcs-studio
+
+# Install YAMCS CFS plugin
+wget http://www.windhoverlabs.com/releases/yamcs-cfs/yamcs-cfs-1.0.1.jar
+sudo mv yamcs-cfs-1.0.1.jar /opt/yamcs/lib
+sudo chown -R root:root /opt/yamcs/lib
 
 # Setup and build the softsim build
 #${CFS_MISSION}/build/softsim/scripts/setup.sh
