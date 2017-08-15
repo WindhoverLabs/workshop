22fa8a74592e4caa968c8a7bd212f714af34a8e3
Mathew Benson <mbenson@windhoverlabs.com>
2017-08-15 02:42:08 +0000

    Removed the vmware provider from the append-to-json.py until vagrant supports vmware packaging.

--- append-to-json.py
+++ append-to-json.py
@@ -7,9 +7,9 @@ import copy
 parser = OptionParser()
 parser.add_option("-f", "--json-file", dest="json_file", help="input/output json file")
 parser.add_option("-u", "--url-vb", dest="url_vb", help="VirtualBox provider file URL, i.e. 'http://mysite/vbox/box_vb_0.1.0.box")
-parser.add_option("-m", "--url-vmware", dest="url-vmware", help="VMWare provider file URL, i.e. 'http://mysite/vbox/box_vmware_0.1.0.box")
+#parser.add_option("-m", "--url-vmware", dest="url-vmware", help="VMWare provider file URL, i.e. 'http://mysite/vbox/box_vmware_0.1.0.box")
 parser.add_option("-c", "--checksum-vb", dest="checksum_vb", help="VirtualBox provider file checksum.  Use the following command to get the sha1... openssl sha1 <box_file>")
-parser.add_option("-w", "--checksum-vmware", dest="checksum_vmware", help="VMWare provider file checksum.  Use the following command to get the sha1... openssl sha1 <box_file>")
+#parser.add_option("-w", "--checksum-vmware", dest="checksum_vmware", help="VMWare provider file checksum.  Use the following command to get the sha1... openssl sha1 <box_file>")
 parser.add_option("-v", "--version", dest="version", help="version, i.e. '0.1.0'")
 
 (options, args) = parser.parse_args()
@@ -17,7 +17,9 @@ parser.add_option("-v", "--version", dest="version", help="version, i.e. '0.1.0'
 with open(options.json_file) as data_file:
     data = json.load(data_file)
 
-new_version = {"version":options.version, "providers":[{"name":"virtualbox", "url":options.url_vb, "checksum_type":"sha1", "checksum":options.checksum_vb},{"name":"vmware_workstation", "url":options.url_vmware, "checksum_type":"sha1", "checksum":options.checksum_vmware}]}
+#new_version = {"version":options.version, "providers":[{"name":"virtualbox", "url":options.url_vb, "checksum_type":"sha1", "checksum":options.checksum_vb},{"name":"vmware_workstation", "url":options.url_vmware, "checksum_type":"sha1", "checksum":options.checksum_vmware}]}
+
+new_version = {"version":options.version, "providers":[{"name":"virtualbox", "url":options.url_vb, "checksum_type":"sha1", "checksum":options.checksum_vb}]}
 
 found = False
 for version in data["versions"]:
