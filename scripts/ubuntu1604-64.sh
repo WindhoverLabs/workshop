29119dc8f533fced02c27afeeb8344f80c2b98eb
Mathew Benson <mbenson@windhoverlabs.com>
2018-01-04 18:24:32 +0000

    Experimental build debugging Gazebo problem.

--- scripts/ubuntu1604-64.sh
+++ scripts/ubuntu1604-64.sh
@@ -9,46 +9,46 @@ cd ..
 WORKSHOP_BASEDIR=$PWD
 
 # Install the prereqs
-export DEBIAN_FRONTEND=noninteractive
-sudo apt-get -y update
-sudo apt-get -y install g++ g++-multilib gcc-multilib git gitk openjdk-8-jdk cinnamon maven libc-bin libc-dev-bin libc6-dev rpm nodejs-legacy npm alien doxygen vagrant libc6-dbg libc6-dbg:i386
-sudo npm -g install bower gulp
+###export DEBIAN_FRONTEND=noninteractive
+###sudo apt-get -y update
+###sudo apt-get -y install g++ g++-multilib gcc-multilib git gitk openjdk-8-jdk cinnamon maven libc-bin ###libc-dev-bin libc6-dev rpm nodejs-legacy npm alien doxygen vagrant libc6-dbg libc6-dbg:i386
+###sudo npm -g install bower gulp
 
 # Setup locales.  The most current Ubuntu 16.04 version has a bug causing the terminal
 # to not open.  These steps will correct it.
-sudo dpkg-reconfigure -f noninteractive tzdata
-sudo locale-gen "en_US.UTF-8"
-sudo dpkg-reconfigure -f noninteractive locales
-sudo localectl set-locale LANG="en_US.UTF-8"
+###sudo dpkg-reconfigure -f noninteractive tzdata
+###sudo locale-gen "en_US.UTF-8"
+###sudo dpkg-reconfigure -f noninteractive locales
+###sudo localectl set-locale LANG="en_US.UTF-8"
 
 # Install Oracle JDK 8
-sudo apt-get install -y default-jre
-sudo apt-get install -y default-jdk
-sudo add-apt-repository -y ppa:webupd8team/java
-sudo apt-get -y update
-echo debconf shared/accepted-oracle-license-v1-1 select true | sudo debconf-set-selections
-echo debconf shared/accepted-oracle-license-v1-1 seen true | sudo debconf-set-selections
-sudo apt-get install -y oracle-java8-installer
-echo 'export JAVA_HOME="/usr/lib/jvm/java-8-oracle"' >> ~/.bashrc
-source ~/.bashrc
+###sudo apt-get install -y default-jre
+###sudo apt-get install -y default-jdk
+###sudo add-apt-repository -y ppa:webupd8team/java
+###sudo apt-get -y update
+###echo debconf shared/accepted-oracle-license-v1-1 select true | sudo debconf-set-selections
+###echo debconf shared/accepted-oracle-license-v1-1 seen true | sudo debconf-set-selections
+###sudo apt-get install -y oracle-java8-installer
+###echo 'export JAVA_HOME="/usr/lib/jvm/java-8-oracle"' >> ~/.bashrc
+###source ~/.bashrc
 
 # Make the desktop look good
-sudo apt-get install -y xubuntu-desktop
-sudo apt-get purge -y ubuntu-desktop unity-*
-sudo apt-get -y autoremove
+###sudo apt-get install -y xubuntu-desktop
+###sudo apt-get purge -y ubuntu-desktop unity-*
+###sudo apt-get -y autoremove
 
 # Install YAMCS
-cd ${WORKSPACE}
-wget http://jenkins.windhoverlabs.lan/releases/yamcs/yamcs-${YAMCS_VERSION}.noarch.rpm
-sudo useradd -r yamcs
-sudo alien --scripts -i yamcs-${YAMCS_VERSION}.noarch.rpm
-sudo chown -R root:root /opt/yamcs
-sudo rm -Rf /opt/yamcs/cache
-sudo rm -Rf /opt/yamcs/etc/*
-sudo cp yamcs/* /opt/yamcs/etc/
-sudo rm -Rf /opt/yamcs/log
-sudo rm -Rf /opt/yamcs/mdb
-rm yamcs-${YAMCS_VERSION}.noarch.rpm
+###cd ${WORKSPACE}
+###wget http://jenkins.windhoverlabs.lan/releases/yamcs/yamcs-${YAMCS_VERSION}.noarch.rpm
+###sudo useradd -r yamcs
+###sudo alien --scripts -i yamcs-${YAMCS_VERSION}.noarch.rpm
+###sudo chown -R root:root /opt/yamcs
+###sudo rm -Rf /opt/yamcs/cache
+###sudo rm -Rf /opt/yamcs/etc/*
+###sudo cp yamcs/* /opt/yamcs/etc/
+###sudo rm -Rf /opt/yamcs/log
+###sudo rm -Rf /opt/yamcs/mdb
+###rm yamcs-${YAMCS_VERSION}.noarch.rpm
 
 # Install YAMCS Studio
 ###wget http://www.windhoverlabs.com/releases/yamcs-studio/yamcs-studio-1.0.0-SNAPSHOT-linux.gtk.x86_64.tar.gz
@@ -58,49 +58,49 @@ rm yamcs-${YAMCS_VERSION}.noarch.rpm
 ###rm yamcs-studio-1.0.0-SNAPSHOT-linux.gtk.x86_64.tar.gz
 
 # Install YAMCS CFS plugin
-cd ${WORKSPACE}
-wget http://jenkins.windhoverlabs.lan/releases/yamcs-cfs/yamcs-cfs-${YAMCS_CFS_VERSION}.jar
-sudo cp yamcs-cfs-${YAMCS_CFS_VERSION}.jar /opt/yamcs/lib
-sudo chown -R root:root /opt/yamcs/lib
-rm yamcs-cfs-${YAMCS_CFS_VERSION}.jar
+###cd ${WORKSPACE}
+###wget http://jenkins.windhoverlabs.lan/releases/yamcs-cfs/yamcs-cfs-${YAMCS_CFS_VERSION}.jar
+###sudo cp yamcs-cfs-${YAMCS_CFS_VERSION}.jar /opt/yamcs/lib
+###sudo chown -R root:root /opt/yamcs/lib
+###rm yamcs-cfs-${YAMCS_CFS_VERSION}.jar
 
 # Install Sage
-git clone git@bitbucket.org:windhoverlabs/sage.git
-sudo cp -R sage /opt/yamcs/
-sudo rm -Rf sage
-cd /opt/yamcs
-sudo chown -R vagrant:vagrant sage
-cd sage
-npm install
-bower install
-ln -s /opt/yamcs/sage/sage /opt/yamcs/sage/node_modules/sage
-cd ..
-sudo chown -R root:root sage
-cd ${WORKSHOP_BASEDIR}
+###git clone git@bitbucket.org:windhoverlabs/sage.git
+###sudo cp -R sage /opt/yamcs/
+###sudo rm -Rf sage
+###cd /opt/yamcs
+###sudo chown -R vagrant:vagrant sage
+###cd sage
+###npm install
+###bower install
+###ln -s /opt/yamcs/sage/sage /opt/yamcs/sage/node_modules/sage
+###cd ..
+###sudo chown -R root:root sage
+###cd ${WORKSHOP_BASEDIR}
 
 # Install Eclipse
-wget http://jenkins.windhoverlabs.lan/external-packages/eclipse/eclipse-cpp-${ECLIPSE_VERSION}.tar.gz
-echo "WORKSHOP_BASEDIR = $WORKSHOP_BASEDIR"
-tar -xzf eclipse-cpp-${ECLIPSE_VERSION}.tar.gz
-sudo mv eclipse /opt/
-sudo chown root:root /opt/eclipse
-echo "export PATH=/opt/eclipse/:$PATH" >> ~/.bashrc
-rm eclipse-cpp-${ECLIPSE_VERSION}.tar.gz
+###wget http://jenkins.windhoverlabs.lan/external-packages/eclipse/eclipse-cpp-${ECLIPSE_VERSION}.tar.gz
+###echo "WORKSHOP_BASEDIR = $WORKSHOP_BASEDIR"
+###tar -xzf eclipse-cpp-${ECLIPSE_VERSION}.tar.gz
+###sudo mv eclipse /opt/
+###sudo chown root:root /opt/eclipse
+###echo "export PATH=/opt/eclipse/:$PATH" >> ~/.bashrc
+###rm eclipse-cpp-${ECLIPSE_VERSION}.tar.gz
 
 # Install additional development tools
-sudo apt-get install -y libc6-dbg:i386 valgrind gcovr lcov xsltproc graphviz  
+###sudo apt-get install -y libc6-dbg:i386 valgrind gcovr lcov xsltproc graphviz  
 
 # Install some nice to have stuff
-sudo apt-get install -y gedit htop gkrellm 
+###sudo apt-get install -y gedit htop gkrellm 
 
 # Configure git 
-sudo apt-get install -y meld
-sudo git config --global push.default simple
-sudo git config --global merge.tool meld
-sudo git config --global  diff.guitool meld
+###sudo apt-get install -y meld
+###sudo git config --global push.default simple
+###sudo git config --global merge.tool meld
+###sudo git config --global  diff.guitool meld
 
 # Install Airliner build dependencies
-sudo apt-get install -y cmake
+###sudo apt-get install -y cmake
 
 # Install TFTP server
 #sudo apt-get install -y xinetd tftpd tftp
@@ -117,7 +117,14 @@ sudo apt-get install -y cmake
 #sudo DEBIAN_FRONTEND=noninteractive dpkg-reconfigure dash
 
 # Install dependencies for PX4 and Gazebo
-#sudo apt-get install -y libimage-exiftool-perl
+###sudo apt-get install -y libprotobuf-dev libprotoc-dev protobuf-compiler libeigen3-dev gazebo7 libgazebo7 libgazebo7-dev
+# To use the camera plugin 
+###sudo apt-get install -y libopencv-dev gstreamer1.0-* libgstreamer1.0-*
+# To use the Gazebo geotagging plugin
+###sudo apt-get install -y libopencv-dev
+
+# Install QGroundControl dependencies
+
 #sudo apt-get install -y catkin
 #sudo apt-get install -y python-jinja2
 #sudo apt-get install -y python-empy
@@ -125,7 +132,6 @@ sudo apt-get install -y cmake
 #sudo -H pip install --upgrade pip 
 #sudo -H pip install --upgrade virtualenv 
 #sudo -H pip install catkin_pkg
-#sudo apt-get install -y gazebo7 libgazebo7 libgazebo7-dev
 #sudo apt-get install -y protobuf-compiler python-protobuf
 #sudo apt-get install -y ant openjdk-8-jdk openjdk-8-jre 
 #sudo apt-get remove -y modemmanager
@@ -138,7 +144,6 @@ sudo apt-get install -y cmake
 #sudo ln -s /usr/bin/arm-linux-gnueabihf-gcc-5 /usr/bin/arm-linux-gnueabihf-gcc
 #sudo ln -s /usr/bin/arm-linux-gnueabihf-g++-5 /usr/bin/arm-linux-gnueabihf-g++
 #sudo apt-get install -y libgstreamer1.0-0 libgstreamer1.0-dev libgstreamer-plugins-base1.0-0 libgstreamer-plugins-base1.0-dev libgstreamer-plugins-good1.0-0 libgstreamer-plugins-good1.0-dev
-#sudo apt-get install -y libimage-exiftool-perl
 
 # Install dependencies for running the ADS-B receiver in Commander
 #sudo apt-get install -y rtl-sdr librtlsdr-dev
